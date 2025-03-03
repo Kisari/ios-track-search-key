72fba4fa1138796bb3dc9584b99635638111e2f9
Kisari <seikakukisari@gmail.com>
2025-03-03 16:16:06 +0700

    Initial Commit

---
+++ ios-track-search-key/ios_track_search_keyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_track_search_keyApp.swift
+//  ios-track-search-key
+//
+//  Created by Minh Truong on 3/3/25.
+//
+
+import SwiftUI
+
+@main
+struct ios_track_search_keyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
